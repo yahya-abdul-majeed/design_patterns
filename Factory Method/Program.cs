64418b4974474473242085df85c79276cbb956c8
YahyaAbdulMajeed <yahya.zf2@gmail.com>
2023-05-17 15:26:44 +0700

    Added factory pattern

---
+++ Factory Method/Program.cs
@@ -0,0 +1,17 @@
+﻿////Factory method is a creational design pattern
+////we don't need to use the 'new' keyword anymore, it is replaced by a call to factory method
+
+//Restaurant beefRes = new BeefBurgerRestaurant();
+//Restaurant chickenRes = new ChickenBurgerRestaurant();
+
+
+//Console.WriteLine(beefRes.createBurger().ToString());
+//Console.WriteLine(chickenRes.createBurger().ToString());
+
+
+Logistics sea = new SeaLogistics();
+Logistics road = new RoadLogistics();
+
+Console.WriteLine(sea.createTransport().ToString());
+
+Console.WriteLine(road.createTransport().ToString());
